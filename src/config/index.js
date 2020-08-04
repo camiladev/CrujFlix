40595a6ab36192b536c350e12c475a496e9c1182
camiladev <camiladev@users.noreply.github.com>
2020-08-04 10:12:58 -0300

    back end

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://mamaflix.herokuapp.com';
-    //'https://mamaflix.herokuapp.com/categorias'; 
+  : 'https://crujflix.herokuapp.com';
+    //'https://crujflix.herokuapp.com/categorias'; 
       //ou http://localhost:8080/categorias maquina local
 
 export default {
